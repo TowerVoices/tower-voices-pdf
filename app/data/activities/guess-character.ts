8feae5add92ed98bc1d058dc35b9ff7574d096ae
TowerVoices <qwdd54125@gmail.com>
2026-06-24 22:21:38 +0300

    حذف الملف الخاطئ

--- app/data/activities/guess-character.ts
+++
@@ -1,7 +0,0 @@
-export default function GuessCharacterPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center text-white bg-black">
-      <h1 className="text-3xl font-bold">هذا التحدي سيكون متاحاً قريباً! ⏳</h1>
-    </div>
-  );
-}
