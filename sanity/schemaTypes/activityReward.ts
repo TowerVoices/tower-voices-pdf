d6ee3720015952ce20edbe3ad0027803acc3b748
TowerVoices <qwdd54125@gmail.com>
2026-06-25 14:54:56 +0300

    حذف الملف الخاطئ

---
+++ sanity/schemaTypes/activityReward.ts
@@ -0,0 +1,36 @@
+import { defineType, defineField } from 'sanity'
+
+export default defineType({
+  name: 'activityReward',
+  title: 'مكافآت الفعاليات (البطاقات)',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'اسم البطاقة',
+      type: 'string',
+    }),
+    defineField({
+      name: 'image',
+      title: 'صورة البطاقة',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'rarity',
+      title: 'الندرة',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'عادي (Common)', value: 'common' },
+          { title: 'نادر (Rare)', value: 'rare' },
+          { title: 'أسطوري (Legendary)', value: 'legendary' }
+        ],
+        layout: 'radio', // لتظهر كخيارات أزرار
+      },
+      description: 'حدد ندرة هذه البطاقة',
+    })
+  ],
+})
--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,11 +2,13 @@ import { type SchemaTypeDefinition } from 'sanity'
 import work from './work' // ملف الروايات الموجود حالياً
 import comment from './comment' // استدعاء ملف التعليقات الجديد
 import activityCharacter from './activityCharacter' // استدعاء ملف شخصيات الفعاليات الجديد
+import activityReward from './activityReward' // استدعاء ملف المكافآت الجديد (البطاقات)
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
     work, 
     comment, 
-    activityCharacter // إضافة activityCharacter هنا هو ما سيجعله يظهر في القائمة
+    activityCharacter,
+    activityReward // إضافة المكافآت هنا هو ما سيجعله يظهر في قائمة الاستوديو
   ], 
 }
